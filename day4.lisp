f91588d9b2cce609ba10e33f73b0c7d5893c45fa
v0sem <progpabsanch@gmail.com>
2023-12-05 12:06:25 +0100

    day 4 1 done, much easier

---
+++ day4.lisp
@@ -0,0 +1,26 @@
+(defvar *input*
+  (uiop:read-file-lines "inputs/day4.txt"))
+
+(defun get-game (gid)
+  (subseq (nth (- gid 1) *input*) 10))
+
+(defun win-count (winning have)
+  (apply #'+ (loop for number in have
+		   when (position number winning)
+		     collect 1)))
+
+(defun get-winning (gid)
+  (mapcar #'parse-integer
+	  (remove "" (uiop:split-string
+	   (subseq (get-game gid) 0 29)) :test #'string=)))
+
+(defun get-numbers (gid)
+  (mapcar #'parse-integer
+	  (remove "" (uiop:split-string
+		      (subseq (get-game gid) 32)) :test #'string=)))
+
+(defun day4-1 ()
+  (apply #'+
+  (loop for gid from 1 to 209
+	when (not (= 0 (win-count (get-winning gid) (get-numbers gid))))
+	collect (expt 2 (- (win-count (get-winning gid) (get-numbers gid)) 1)))))
