1b51fd73e1b3d7a98b078d64d9516baf2f6abc77
v0sem <progpabsanch@gmail.com>
2023-12-05 23:14:30 +0100

    day4 done, much easier

--- day4.lisp
+++ day4.lisp
@@ -24,3 +24,21 @@
   (loop for gid from 1 to 209
 	when (not (= 0 (win-count (get-winning gid) (get-numbers gid))))
 	collect (expt 2 (- (win-count (get-winning gid) (get-numbers gid)) 1)))))
+
+(defun card+ (card-a num)
+  (list (car card-a) (+ (nth 1 card-a) num)))
+
+(defun init-list ()
+  (loop for gid from 1 to 209
+	collect (list gid 1)))
+
+(defun process-list ()
+  (let ((l (init-list)))
+    (loop for gid from 1 to 209
+	  do (loop for gid2 from gid to (+ (win-count (get-winning gid) (get-numbers gid)) (- gid 1))
+					do (setf (nth gid2 l)
+						 (card+ (nth gid2 l) (nth 1 (nth (- gid 1) l))))))
+    l))
+
+(defun day4-2 ()
+  (apply #'+ (mapcar (lambda (x) (nth 1 x)) (process-list))))
