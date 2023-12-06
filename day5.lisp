5b06adbbcf57ace140d265cb5f3695384af400f0
v0sem <progpabsanch@gmail.com>
2023-12-06 16:28:53 +0100

    my greatest fear, it doesnt work

--- day5.lisp
+++ day5.lisp
@@ -8,11 +8,75 @@
 (defun parse-line (str)
   (mapcar #'parse-integer (uiop:split-string str)))
 
-(defun get-maps (line)
-  (let ((source (get-range-as-list (nth 1 line) (nth 2 line)))
-	(destination (get-range-as-list (car line) (nth 2 line))))
-    (mapcar #'list source destination)))
+(defun parse-map (num maps)
+  (loop for map in maps
+	when (and
+	      (> (+ (cadr map) (caddr map)) num)
+	      (>= num (cadr map)))
+	  return (+ (car map) (- num (cadr map)))
+	))
 
-(defun seed-to-soil-map ()
+(defun seed-to-soil ()
   (loop for x from 3 to 49
-	append (get-maps (parse-line (nth x *input*)))))
+	collect (parse-line (nth x *input*))))
+
+(defun soil-to-fert ()
+  (loop for x from 52 to 83
+	collect (parse-line (nth x *input*))))
+
+(defun fert-to-water ()
+  (loop for x from 86 to 95
+	collect (parse-line (nth x *input*))))
+
+(defun water-to-light ()
+  (loop for x from 98 to 123
+	collect (parse-line (nth x *input*))))
+
+(defun light-to-temp ()
+  (loop for x from 126 to 151
+	collect (parse-line (nth x *input*))))
+
+(defun temp-to-humid ()
+  (loop for x from 154 to 193
+	collect (parse-line (nth x *input*))))
+
+(defun humid-to-loc ()
+  (loop for x from 196 to 236
+	collect (parse-line (nth x *input*))))
+
+(defun h2l (num)
+  (if (parse-map num (humid-to-loc))
+      (parse-map num (humid-to-loc))
+      num))
+
+(defun t2l (num)
+  (if (parse-map num (temp-to-humid))
+      (h2l (parse-map num (temp-to-humid)))
+      (h2l num)))
+
+(defun l2l (num)
+  (if (parse-map num (light-to-temp))
+      (t2l (parse-map num (light-to-temp)))
+      (t2l num)))
+
+(defun w2l (num)
+  (if (parse-map num (water-to-light))
+      (l2l (parse-map num (water-to-light)))
+      (l2l num)))
+
+(defun f2l (num)
+  (if (parse-map num (fert-to-water))
+      (w2l (parse-map num (fert-to-water)))
+      (w2l num)))
+
+(defun s2l (num)
+  (if (parse-map num (soil-to-fert))
+      (f2l (parse-map num (soil-to-fert)))
+      (f2l num)))
+
+(defun get-seeds ()
+  (mapcar #'parse-integer (uiop:split-string (subseq (car *input*) 7))))
+
+(defun day5-1 ()
+  (loop for seed in (get-seeds)
+	minimize (s2l seed)))
